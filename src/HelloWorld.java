1a4602725e7d5ca1a8cd6afe6add4ca5e072dbfd
SkilStak Coding Arts <learn@skilstak.com>
2015-08-29 17:06:11 -0400

    save

---
+++ src/HelloWorld.java
@@ -0,0 +1,6 @@
+import skilstak.c;
+public class HelloWorld {
+   public static void main(String[] args) {
+    System.out.println(c.cl + c.y + "but do banana exist\n" + c.b + "Hello World!" + c.x); 
+  }
+}
---
+++ src/hello2.java
@@ -0,0 +1,7 @@
+import skilstak.c;
+
+public class hello2 {
+    public static void main(String[] args) {
+      System.out.println("no");
+    }    
+}
