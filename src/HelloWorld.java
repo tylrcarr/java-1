5fadb0caa13cdf45aa646460ddb7559fb508a38d
Tyler <tylercarr@skilstak.sh>
2015-09-05 17:12:45 -0400

    save

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,19 @@
 import skilstak.c;
+import java.util.*;
 public class HelloWorld {
    public static void main(String[] args) {
-    System.out.println(c.cl + c.y + "but do banana exist\n" + c.b + "Hello World!" + c.x); 
-  }
+    System.out.println(c.cl + c.y + "but do banana exist\n" + c.b + "answer the question\n" + c.x); 
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Yes or no.");
+    String name = scanner.nextLine();
+    while(true){
+        if(name.equalsIgnoreCase("yes") || name.equalsIgnoreCase("y")){
+         System.out.println("No.");
+         break;
+        } else if(name.equalsIgnoreCase("no") || name.equalsIgnoreCase("n"));{
+         System.out.println("Correct.");
+         break;
+        }
+    }
+   }
 }
---
+++ src/HelloWorld2.java
@@ -0,0 +1,8 @@
+import skilstak.c;
+public class HelloWorld2 { 
+   public static void main(String[] args){
+    while (true) {
+        System.out.print(c.rc() + "nyan");
+    } 
+    }
+}
