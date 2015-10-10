53408ef81f70168d68bcc6f812910e4adb015ade
Tyler Carr <tylrcarr@gmail.com>
2015-10-10 17:12:59 -0400

    save

--- SkilMon/fooget.java
+++ SkilMon/fooget.java
@@ -6,11 +6,16 @@ import java.io.*;
 
 public class fooget {
     public static void main(String[] args) throws IOException {
+          
         URL url = new URL("http://pokeapi.co/api/v1/pokemon/" + args[0]);
         URLConnection conn = url.openConnection();
         Map data = new Gson().fromJson(new InputStreamReader(conn.getInputStream()), Map.class);
         //System.out.println(data); 
-        System.out.println(data.get("speed"));
+        System.out.println(c.b + args[0] + "'s " + args[1] + " is/are " + c.x); 
+        String before = data.get(args[1]).toString();
+        for(int i=1, i < before.length(), i++){
+            if before.charAt(i);
+        }
     }
 
 
